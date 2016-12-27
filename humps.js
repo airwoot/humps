84d92bacb97cc81d3584b298a736c445a80468e1
Arijit Bhattacharya <hoodwink73@live.com>
2016-12-27 11:49:59 +0530

    Fix typo

--- humps.js
+++ humps.js
@@ -79,7 +79,7 @@
     return obj === Object(obj);
   };
   var _isArray = function(obj) {
-    return Array.isArray';
+    return Array.isArray(obj);
   };
   var _isDate = function(obj) {
     return toString.call(obj) == '[object Date]';
